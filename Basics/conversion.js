e6bcef4c95625d8ca88336142b1f2ad3d7f0eb81
Vallabh Jagtap <95693051+Vjgatap@users.noreply.github.com>
2024-11-08 08:00:31 +0000

    Final!

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -1,20 +1,17 @@
 let score = 33
-console.log(typeof score);
-console.log(typeof(score));
+console.log("type of score variable is : "+typeof(score));
 
 let score1 = "44"
-console.log(typeof score1);
-console.log(typeof (score1));
+console.log("type of score1 variable is : "+typeof (score1));
 
 // let's start to conversion string - num / number - string
 console.log();
 
+let valStrNum = String(score);
+console.log("type after conversion int is score : "+typeof(valStrNum));
 
 let valIntNum = Number(score1);
-console.log("Value after conversion string is : "+typeof (valIntNum));
-
-let valStrNum = String(score);
-console.log("Value after conversion int is : "+typeof(valStrNum));
+console.log("type after conversion string score1 is : "+typeof (valIntNum));
 
 console.log();
 
